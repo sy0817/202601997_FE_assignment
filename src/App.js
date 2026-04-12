1a48583994c89812310511bc1622375d312d0592
sy0817 <yetsgz0535158@naver.com>
2026-04-12 17:08:14 +0900

    easy-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// 📄 src/App.js
+
+import ProfileCard from './ProfileCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '60px' }}>
+      <ProfileCard
+        name="최서연"
+        age={20}
+        dream="잠자기"
+        hobby="잠자기"
+        mbti="INTP"
+      />
     </div>
   );
 }
